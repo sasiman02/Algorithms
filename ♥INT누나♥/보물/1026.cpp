4e9b2f3109b89f6cfe63df86c9dd99affa4f2f17
sasiman02 <gustndlfrl@gmail.com>
2018-12-29 17:16:35 +0900

    add 1026

--- ♥INT누나♥/보물/1026.cpp
+++ ♥INT누나♥/보물/1026.cpp
@@ -1,36 +1,104 @@
 #include <iostream>
 using namespace std;
 
-int f(int A[], int B[]) {
-	int N = sizeof(A);
-	int S[N];
+void swap(int &a, int &b) {
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void bubblesort(int *A, int N) {
+		
+	for(int i = 0; i < N ; i++) {
+		for(int j = 0; j < N - i - 1; j++)	{
+			if( A[j] > A[j + 1]) {
+				swap(A[j], A[j + 1]);
+			}
+		}
+	}
+}
+
+void f(int A[], int B[],const int N) {
+	int S = 0;
+	
+	int rankB[N];
+	
+	bubblesort(A, N);
+//	bubblesort(B, N);
+//	cout << "Bubble result" << endl;
+//	
+//	for(int i = 0; i < N; i++) {
+//		cout << A[i] << endl;
+//	}
+//	cout << "Bubble result" << endl;
+	
+	for(int i = 0; i < N; i++) {
+		rankB[i] = N;
+	}
 	
 	for(int i = 0; i < N; i++) {
 		for(int j = 0; j < N; j++) {
-			S[i] += A[j]*B[j];
+			if(B[i] >= B[j] ) {
+				rankB[i]--;
+			}
 		}
-		
-	} 
-	int min;
+	}
 	
-	return min;
+//	cout << "rank" << endl;
+//	for(int i = 0; i < N; i++) {
+//		cout << rankB[i] << endl;
+//	}
+//	cout << "rank" << endl;
+//	int A1[N];
+	
+//	for(int i = 0; i < N; i++) {
+//		for(int j = 0; j < N; j++) {
+//			if(rankB[j] == i) {
+//				A1[j] = A[i];
+//			}
+//		}
+//	}
+	
+//	for(int i = 0; i < N; i++) {
+//		cout << A1[i] << endl;
+//	}
+	
+	for(int i = 0; i < N; i++) {
+		for(int j = 0; j < N; j++) {
+			if((rankB[j] == i)) {
+				S += A[i]*B[j];	
+			}
+		}
+	}
+	
+
+//	for(int i = 0; i < N; i++) {
+//		S += A[i]*B[N - 1 - i];
+//	}
+	
+	cout << S;
 }
+
+
 
 int main() {
 	int N;
 	
 	cin >> N;
 	
-	int A[N];
-	int B[N];
-	
+	int A[51];
+	int B[51];
+
+
+		
 	for(int i = 0; i < N; i++) {
 		cin >> A[i];
 	}
-	
+
 	for(int i = 0; i < N; i++) {
 		cin >> B[i];
 	}
 	
-	f(A,B);
+	
+	f(A,B,N);
 }
