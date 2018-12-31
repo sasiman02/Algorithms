d539c04ac98382a2544c4fa17593ee04dd2e34c4
sasiman02 <gustndlfrl@gmail.com>
2018-12-31 18:20:03 +0900

    add 2167

--- ♥INT누나♥/2차원 배열의 합/2167.cpp
+++ ♥INT누나♥/2차원 배열의 합/2167.cpp
@@ -3,50 +3,59 @@ using namespace std;
 
 int main() {
 	int N, M;
+	int arr[300][300];
 	
 	cin >> N >> M;
 	
-	int arr[N][M];
-	for(int j = 0; j < N; j++) {
-		for(int i = 0; i < M; i++) {
-			cin >> arr[j][i];
+	
+	for(int i = 0; i < N; i++) {
+		for(int j = 0; j < M; j++) {
+			cin >> arr[i][j];	
 		}
 	}
 	
-	int K;
+	int T;
+	int O[10000];
 	
-	cin >> K;
+	cin >> T;
 	
-	int sum[K] = {};
+	int tmp;
 	
-	for(int n = 0; n < K; n++) {
-		int i, j , x , y;
-		
+	int i, j, x, y;
+	
+	for(int t = 0; t < T; t++) {
 		cin >> i >> j >> x >> y;
-
-		if(j != y) {
-			for(;i - 1 < N; i++) {
-				sum[n] += arr[i - 1][j - 1];
+//		tmp = j;
+//		for(; i <= x; i++) {
+//			if(i < x) {
+//				for(; j <= M; j++) {
+//					O[t] += arr[i - 1][j - 1];
+//				}
+//				j = tmp;	
+//			}
+//		
+//			else if(i == x) {
+//				for(; j <= y; j++) {
+//					O[t] += arr[i - 1][j - 1];
+//				}
+//			}
+//		}
+		for(; j <= y; j++) {
+			if(j < y) {
+				for(;i <= N; i++) {
+					O[t] += arr[i - 1][j - 1];
+				}
+				i = 1;
 			}
-			
-			for(;j < y - 1; j++) {
-				for(int s = 0; s < N; s++) {
-					sum[n] += arr[s][j];
+			else if(j == y) {
+				for(;i <= x; i++) {
+					O[t] += arr[i - 1][j - 1];
 				}
 			}
-			
-			for(int s = 0; s < x; s++) {
-				sum[n] += arr[s][y - 1];
-			}
-		}
-		else {
-			for(; i - 1 < x; i++) {
-					sum[n] += arr[i - 1][j - 1];
-			}
 		}
 	}
 	
-	for(int n = 0; n < K; n++) {
-		cout << sum[n] << endl;
-	}	
+	for(int t = 0; t < T; t++) {
+		cout << O[t] << endl;
+	}
 }
