51185a3941c690cdfbc5ca4b74b43d19a3e61e48
sasiman02 <gustndlfrl@gmail.com>
2019-01-09 23:24:47 +0900

    complete 1075

--- ♥INT누나♥/나누기/1075.cpp
+++ ♥INT누나♥/나누기/1075.cpp
@@ -3,9 +3,40 @@ using namespace std;
 
 int main() {
 	int N, F;
-	
+	int s, q; // s ³ª¸ÓÁö q ¸ò 
+	 
 	cin >> N;
 	cin >> F;
 	
-	 N + F - (N % F)
+
+	int a = N % 100;
+	
+	for(int i = 0; a - i >= 0; i++) {
+		if((N - a + i) % F == 0) {
+			if((N - a + i) % 100 < 10){
+				cout << '0' << (N - a + i)%100;
+				return 0;
+			}
+			else {
+				cout <<(N - a + i)%100;
+				return 0;
+			}	
+		}
+	}
+	
+	for(int i = 0; i <= 99 - a; i++) {
+		if((N + i) % F == 0) {
+			if((N + i) % 100 < 10){
+				cout << '0' << (N - a + i)%100;
+				return 0;
+			}
+			else {
+				cout <<(N + i)%100;
+				return 0;
+			}				
+		}
+	}
+
+	
+
 }
