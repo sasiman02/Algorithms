cab80d560fd814898b3f15c0ff055f41d1e1c51c
sasiman02 <gustndlfrl@gmail.com>
2019-03-21 01:19:56 +0900

    1463

--- ♥INT누나♥/팩토리얼 0의 개수/1676.cpp
+++ ♥INT누나♥/팩토리얼 0의 개수/1676.cpp
@@ -1,11 +1,38 @@
 #include<iostream>
-#include<cmath>
 using namespace std;
-
+int fun(int n, int s) {
+	int cnt = 0;
+	
+	while(1) {
+		if(n%s == 0) {
+			cnt++;
+			n = n / s;
+		}
+		else {
+			break;
+		}
+	}
+	return cnt;
+}
 int main() {
-	int N;
+	int n;
+	cin >> n;
+	int cnt2 = 0;
+	int cnt5 = 0;
 	
-	cin >> N;
+	for(int i = 1; i <= n; i++) {
+		if(i%2 == 0) {
+			cnt2 += fun(i,2);
+		}
+		if(i%5 == 0) {
+			cnt5 += fun(i,5);
+		}
+	}
 	
-	
+	if(cnt2 > cnt5) {
+		cout << cnt5 << endl;
+	}
+	else {
+		cout << cnt2 << endl;
+	}
 }
